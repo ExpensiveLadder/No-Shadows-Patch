71e36159a5a2520bc83da9b3990380c1666e253e
ExpensiveLadder <daniel52042@hotmail.co.nz>
2021-03-02 11:25:04 +1300

    commit

---
+++ NoShadowsPatch/Program.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Synthesis;
+using Mutagen.Bethesda.Skyrim;
+using System.Threading.Tasks;
+
+namespace NoShadowsPatch
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            return await SynthesisPipeline.Instance
+                .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
+                .Run(args, new RunPreferences()
+                {
+                    ActionsForEmptyArgs = new RunDefaultPatcher()
+                    {
+                        IdentifyingModKey = "YourPatcher.esp",
+                        TargetRelease = GameRelease.SkyrimSE,
+                    }
+                });
+        }
+
+        public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
+        {
+            foreach (var lightGetter in state.LoadOrder.PriorityOrder.Light().WinningOverrides())
+            {
+                if (lightGetter.Flags.HasFlag(Light.Flag.ShadowHemisphere) || lightGetter.Flags.HasFlag(Light.Flag.ShadowOmnidirectional) || lightGetter.Flags.HasFlag(Light.Flag.ShadowSpotlight)) {
+                    var light = state.PatchMod.Lights.GetOrAddAsOverride(lightGetter);
+
+                    if (lightGetter.Flags.HasFlag(Light.Flag.ShadowHemisphere)) {
+                        light.Flags -= Light.Flag.ShadowHemisphere;
+                    }
+
+                    if (lightGetter.Flags.HasFlag(Light.Flag.ShadowOmnidirectional)) {
+                        light.Flags -= Light.Flag.ShadowOmnidirectional;
+                    }
+     
+                    if (lightGetter.Flags.HasFlag(Light.Flag.ShadowSpotlight)) {
+                        light.Flags -= Light.Flag.ShadowSpotlight;
+                    }
+                }
+            }        
+        }
+    }
+}
